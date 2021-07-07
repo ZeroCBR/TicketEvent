83b39061a168fb0725995c115e01e52c9eaaced1
Ray Chen <ray.chen@mondo.com.au>
2021-07-08 00:17:14 +1000

    routing and component and service register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EventDetailComponent } from './event-detail/event-detail.component';
+import { EventListComponent } from './event-list/event-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: EventListComponent},
+  {path:'event/:id', component: EventDetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ApiKeyInterceptor } from './core/interceptors/api-key-interceptor';
+import { ConfigService } from './core/services/config.service';
+import { EventService } from './core/services/event.service';
+import { EventDetailComponent } from './event-detail/event-detail.component';
+import { EventListComponent } from './event-list/event-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventDetailComponent,
+    EventListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    EventService,
+    ConfigService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ApiKeyInterceptor,
+      multi: true
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
