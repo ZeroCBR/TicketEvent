c003a2125dee2502ba1318edcbe9d8419292f8e1
Ray Chen <ray.chen@mondo.com.au>
2021-07-08 00:15:44 +1000

    event detail

--- src/app/event-detail/event-detail.component.ts
+++ src/app/event-detail/event-detail.component.ts
@@ -1,12 +1,29 @@
-import {Component} from '@angular/core'
+import {Component, OnDestroy, OnInit} from '@angular/core'
+import { ActivatedRoute } from '@angular/router'
+import { Subscription } from 'rxjs';
+import { TicketEvent } from '../core/models/event.model';
+import { EventService } from '../core/services/event.service'
 
 @Component({
   selector: 'event-detail',
   styleUrls: ['./event-detail.component.scss'],
   templateUrl: './event-detail.component.html'
 })
-export class EventDetailComponent {
-  constructor() {
-    
+export class EventDetailComponent implements OnInit, OnDestroy {
+  private routeSub: Subscription;
+  event: TicketEvent; 
+
+  constructor(private eventService:EventService, private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.routeSub = this.route.params.subscribe(params => {
+      this.eventService.getEventById(params['id']).subscribe(data=>{
+        this.event = data
+      })
+    });
+  }
+
+  ngOnDestroy(){
+    this.routeSub.unsubscribe();
   }
 }
