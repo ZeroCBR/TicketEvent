20adb1bdb2deeb9775a6a8588683dfbe03b1b029
Ray Chen <ray.chen@mondo.com.au>
2021-07-07 21:51:40 +1000

    create event detail component

---
+++ src/app/event-detail/event-detail.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core'
+
+@Component({
+  selector: 'event-detail',
+  styleUrls: ['./event-detail.component.scss'],
+  templateUrl: './event-detail.component.html'
+})
+export class EventDetailComponent {
+  constructor() {
+    
+  }
+}
