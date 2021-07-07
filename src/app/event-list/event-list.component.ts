d9804f8164d6d6b306a48cc3472371f27f9e0d2f
Ray Chen <ray.chen@mondo.com.au>
2021-07-07 21:51:55 +1000

    create event list component

---
+++ src/app/event-list/event-list.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core'
+
+@Component({
+  selector: 'event-list',
+  styleUrls: ['./event-list.component.scss'],
+  templateUrl: './event-list.component.html'
+})
+export class EventListComponent {
+  constructor() {
+    
+  }
+}
