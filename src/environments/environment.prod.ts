a3080ea5d632fa1317aec551d1b50a3ac3a2cbdd
Ray Chen <ray.chen@mondo.com.au>
2021-07-08 12:52:52 +1000

    deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: 'WOScmRIP2e8GNFlcVo0GsF5YSuu28cGt',
+  ticketMasterUrl: 'https://app.ticketmaster.com/discovery/v2'
 };
